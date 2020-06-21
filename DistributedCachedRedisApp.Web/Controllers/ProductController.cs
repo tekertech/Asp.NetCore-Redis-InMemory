c754782772f11b43bb99105558d969254107aac1
Mehmet Teker <teker.mehmed@gmail.com>
2020-06-21 18:01:29 +0300

    StackExchangeRedisCache nuget paketi ile, Redis Cache projesi eklendi.

---
+++ DistributedCachedRedisApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace DistributedCachedRedisApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
