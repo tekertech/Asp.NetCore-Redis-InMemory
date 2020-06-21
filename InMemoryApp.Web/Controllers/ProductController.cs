12a460216c5318d8d62fb6c22b73a9eaea381132
Mehmet Teker <teker.mehmed@gmail.com>
2020-06-21 11:45:24 +0300

    IMemoryCache interface nin Get,Set,GetOrCreate,TryGetValue metotlarının kullanımı.

---
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IMemoryCache memoryCache;
+
+        public ProductController(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            //1. yol
+            if (String.IsNullOrEmpty(memoryCache.Get<string>("zaman")))
+            {
+                memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+
+            //2. yol
+
+            if(memoryCache.TryGetValue("zaman", out object result))
+            {
+                memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+            
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            memoryCache.GetOrCreate<string>("zaman",  cacheEntry => {
+                return DateTime.Now.ToString();
+            }
+            );
+             
+            ViewBag.zaman = memoryCache.Get("zaman");
+            return View();
+        }
+    }
+}
