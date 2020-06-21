2118946211aa5b34c9a425ca4d57d554c42a127b
Mehmet Teker <teker.mehmed@gmail.com>
2020-06-21 12:35:41 +0300

    Complex types caching with InMemoryCache

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InMemoryApp.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 
@@ -20,16 +21,20 @@ namespace InMemoryApp.Web.Controllers
         {
             //if(memoryCache.TryGetValue("zaman", out object result))
             //{
-                MemoryCacheEntryOptions memoryCacheEntryOptions = new MemoryCacheEntryOptions();
-                memoryCacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
-                memoryCacheEntryOptions.SlidingExpiration = TimeSpan.FromSeconds(10);
-                memoryCacheEntryOptions.Priority = CacheItemPriority.Normal;
+            MemoryCacheEntryOptions memoryCacheEntryOptions = new MemoryCacheEntryOptions();
+            memoryCacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+            memoryCacheEntryOptions.SlidingExpiration = TimeSpan.FromSeconds(10);
+            memoryCacheEntryOptions.Priority = CacheItemPriority.Normal;
             // (object key, object value, EvictionReason reason, object state);
-               memoryCacheEntryOptions.RegisterPostEvictionCallback((key, value, reason ,state) =>
-               {
-                   memoryCache.Set<string>("callbackEviction", $" {key} -> {value} => Sebeb : {reason}");
-               });
-                memoryCache.Set<string>("zaman", DateTime.Now.ToString(),memoryCacheEntryOptions);
+            memoryCacheEntryOptions.RegisterPostEvictionCallback((key, value, reason, state) =>
+            {
+                memoryCache.Set<string>("callbackEviction", $" {key} -> {value} => Sebeb : {reason}");
+            });
+            memoryCache.Set<string>("zaman", DateTime.Now.ToString(), memoryCacheEntryOptions);
+
+            Product product = new Product() { Id = 1, Name = "Kalem", Price = 10 };
+            memoryCache.Set<Product>("product:1", product);
+
             //}
             return View();
         }
@@ -40,6 +45,8 @@ namespace InMemoryApp.Web.Controllers
             memoryCache.TryGetValue("callbackEviction", out string callbackCache);
             ViewBag.zaman = zamanCache;
             ViewBag.callback = callbackCache;
+            memoryCache.TryGetValue("product:1", out Product productCache);
+            ViewBag.product = productCache;
             return View();
         }
     }
