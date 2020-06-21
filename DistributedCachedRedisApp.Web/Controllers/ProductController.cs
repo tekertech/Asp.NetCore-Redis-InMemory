df485a128db8fe9b3e4cbf57d7382e32d707618b
Mehmet Teker <teker.mehmed@gmail.com>
2020-06-21 22:20:52 +0300

    Complex Type ların cahlenmesi.

--- DistributedCachedRedisApp.Web/Controllers/ProductController.cs
+++ DistributedCachedRedisApp.Web/Controllers/ProductController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using DistributedCachedRedisApp.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 
 namespace DistributedCachedRedisApp.Web.Controllers
 {
@@ -18,8 +21,27 @@ namespace DistributedCachedRedisApp.Web.Controllers
 
         public IActionResult Index()
         {
+            DistributedCacheEntryOptions options = new DistributedCacheEntryOptions();
+            options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+            Product product = new Product { Id = 1, Name = "Mehmet", Price = 10 };
+            string jsonProduct = JsonConvert.SerializeObject(product);
+            distributedCache.SetStringAsync($"product:{product.Id.ToString()}", jsonProduct,options);
 
             return View();
         }
+
+        public IActionResult Show()
+        {
+            string product = distributedCache.GetString("product:1");
+            ViewBag.product = product;
+            return View();
+        }
+
+        public IActionResult Remove()
+        {
+            distributedCache.Remove("product:1");
+            return View();
+        }
+
     }
 }
