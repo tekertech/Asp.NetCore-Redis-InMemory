a9736fd7ae8b61aea7b272a5839443d8e465d60f
Mehmet Teker <teker.mehmed@gmail.com>
2020-06-24 23:26:15 +0300

    HashType ile ilgili örnek yapıldı.

---
+++ RedisExchangeAPI.Web/Controllers/HashTypeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class HashTypeController : BaseController
+    {
+        public string hashKey { get; set; } = "sozluk";
+        public HashTypeController(RedisService redisService, int dbCount = 6) : base(redisService, dbCount)
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            Dictionary<string, string> list = new Dictionary<string, string>();
+
+            if (db.KeyExists(hashKey))
+            {
+                db.HashGetAll(hashKey).ToList().ForEach(x =>
+                {
+                    list.Add(x.Name, x.Value);
+                });
+            }
+
+            return View(list);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name, string value)
+        {
+            db.HashSet(hashKey, name, value);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteItem(string name)
+        {
+            db.HashDelete(hashKey, name);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
--- RedisExchangeAPI.Web/Startup.cs
+++ RedisExchangeAPI.Web/Startup.cs
@@ -52,7 +52,7 @@ namespace RedisExchangeAPI.Web
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=SortedSetType}/{action=Index}/{id?}");
+                    pattern: "{controller=HashType}/{action=Index}/{id?}");
             });
         }
     }
